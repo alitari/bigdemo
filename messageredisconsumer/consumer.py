e7f5d9f5efe278aed2a9c9fbad5953fbc86f8e10
alitari <alexander.krieg@arcor.de>
2018-08-05 21:56:00 +0200

    save redis words

--- messageredisconsumer/consumer.py
+++ messageredisconsumer/consumer.py
@@ -2,10 +2,21 @@
 import pika
 import redis
 import os
+import json
 
+
+with open('english_words.txt') as f:
+    english_words = f.readlines()
+    english_words = [x.strip() for x in english_words]
+
+
+gap_words = ['and', 'or', '.']
+message_id_key = 'message_id'
 rabbitmq_host = os.environ['RABBITMQ_HOST']
 rabbitmq_user = os.environ['RABBITMQ_USER']
 rabbitmq_password = os.environ['RABBITMQ_PASSWORD']
+
+r = redis.Redis(host='bigdemo-redis-master', password='redis')
 
 queue_name = os.environ['RABBITMQ_MESSAGE_QUEUE']
 exchange_name = os.environ['RABBITMQ_EXCHANGE']
@@ -26,7 +37,30 @@ channel.queue_bind(exchange=exchange_name,
 
 
 def message_received(ch, method, properties, body):
-    print(" Received %r" % body)
+
+    m_dict = json.loads(body)
+
+    m_id = store_message(m_dict)
+
+    words_all = m_dict['text'].split()
+    words_filtered = [w for w in words_all if w in english_words]
+
+    for word in words_filtered:
+        store_word(word, m_id)
+
+
+def store_message(json):
+    m_text = json['text']
+    m_id = r.incr(message_id_key)
+    print(" Received message text: %s" % m_text)
+    if r.set(m_id, m_text):
+        print(" and stored with id %s" % m_id)
+    return m_id
+
+
+def store_word(word, m_id):
+    if r.rpush(word, m_id):
+        print(" stored word %s" % word)
 
 
 print('consuming queue \'% s\' bound to exchange \'% s\' ...' %
